b8abfc742e9f3f39bb19c2d9dcc4b1570d6a211e
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-06-06 11:22:24 +0300

    added ModelSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import ModelSection from "./components/ModelSection";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <ModelSection />
     </main>
   );
 }
